4466af13430e06df21d7ace2858ec0286fce020b
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-10-01 15:45:19 -0300

    createcategoryservice.ts

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,27 @@
 import { Router } from "express";
 
-import Category from "../model/Category";
+import CategoriesRepository from "../repositories/CategoriesRepository";
+
+import CreateCategoryService from "../service/CreateCategoryService";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
 
-Object.assign(category, { 
-  name, 
-  description,
-  created_at: new Date()
+ createCategoryService.execute({ name, description });
+
+  return response.status(201).send();
 });
 
-  categories.push(category);
+categoriesRoutes.get("/", (request, response) => {
+  const all = categoriesRepository.list();
 
-  return response.status(201).json({ category });
+  return response.json(all);
 });
 
 export default categoriesRoutes;
+ 
