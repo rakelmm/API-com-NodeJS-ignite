cfd97df8703d2253aa73c1ae56976debd2b0466c
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-11-11 21:54:09 -0300

    entities

--- src/modules/cars/repositories/implementations/SpecificationsRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationsRepository.ts
@@ -1,5 +1,5 @@
 
-import { Specification } from "../../model/Specification";
+import { Specification } from "../../entities/Specification";
 import { ISpecificationsRepository, ICreateSpecificationDTO } from "../ISpecificationsRepository";
 
 class SpecificationsRepository implements ISpecificationsRepository {
--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,16 +1,16 @@
 import { Response, Request } from "express";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
-class CreatecategoryController {
+class CreateCategoryController {
   constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
 
-  handle(request: Request, response: Response): Response {
+  async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
 
-    this.createCategoryUseCase.execute({ name, description });
+    await this.createCategoryUseCase.execute({ name, description });
   
     return response.status(201).send();
   }
 }
 
-export { CreatecategoryController };
+export { CreateCategoryController };
