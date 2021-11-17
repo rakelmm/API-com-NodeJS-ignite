eee001d8fb0f03cb098d61c3c640a9b54ab96a91
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-11-17 15:40:28 -0300

    criptografia e senha

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -4,11 +4,9 @@ import { container } from "tsyringe";
 import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoryController {
-
-
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
-
+    
     const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
 
     await createCategoryUseCase.execute({ name, description });
