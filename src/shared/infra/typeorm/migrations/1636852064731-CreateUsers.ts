3d7b0fe2ab8793e03eef8df33a827d866eb4fbe4
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2021-12-20 15:09:23 -0300

    CreateRentals.ts

--- src/shared/infra/typeorm/migrations/1636852064731-CreateUsers.ts
+++ src/shared/infra/typeorm/migrations/1636852064731-CreateUsers.ts
@@ -9,7 +9,8 @@ export class CreateUsers1636852064731 implements MigrationInterface {
                 columns: [
                     {
                         name: "id",
-                        type: "uuid"
+                        type: "uuid",
+                        isPrimary: true,
                     },
                     {
                         name: "name",
