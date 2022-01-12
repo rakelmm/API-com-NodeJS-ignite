85eb326fcf10bc4fd8d33e53b615098e324be085
Rakel Moreira <rakelmoreiramendonca@gmail.com>
2022-01-12 12:33:59 -0300

    SendForgotPasswordMailUseCase.ts

--- src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -4,14 +4,16 @@ import { getRepository, Repository } from "typeorm";
 
 import { UserTokens } from "../entities/UserTokens";
 
-
 class UsersTokensRepository implements IUsersTokensRepository {
   private repository: Repository<UserTokens>;
 
   constructor() {
     this.repository = getRepository(UserTokens);
   }
-
+  findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens> {
+    throw new Error("Method not implemented.");
+  }
+  
   async create({ 
     expires_date, 
     refresh_token, 
@@ -27,7 +29,16 @@ class UsersTokensRepository implements IUsersTokensRepository {
 
    return userToken
   }
-  
+   async findByUserId(user_id: string, refresh_token: string): Promise<UserTokens> {
+    const usersTokens = await this.repository.findOne({
+      user_id,
+      refresh_token
+    });
+    return usersTokens;
+  }
+  async deleteById(id: string): Promise<void> {
+   await this.repository.delete(id);
+  }
 }
 
 export { UsersTokensRepository }
